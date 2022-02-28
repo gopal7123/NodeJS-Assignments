5242c484099668d84dd9aac8eda1facf94546b6b
gopal7123 <jatavathgopalkrishna123@gmail.com>
2022-02-28 17:35:06 +0530

    Assignment-1 Submission

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,14 +1,21 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
-}
+    return process.argv[5]
+};
 
 function getNameFromEnv() {
     // Write your code here
-}
+    return process.env.name
+};
 
 function getNameFromReadLine() {
     // Write your code here
-}
+    const readLine = require("readLine");
+    const rl = readLine.createInterface({
+        input:process.stdin,
+        output:process.stdout
+    });
+};
 
 module.exports = {
     getNameFromCommandLine,
